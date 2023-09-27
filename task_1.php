e89183ad077c2f30fc6611b42ae25463bb9d115b
Shopify Expert <134607181+shopifyexpert8@users.noreply.github.com>
2023-09-27 23:25:14 +0600

    assignment 3 task one

---
+++ task_1.php
@@ -0,0 +1,12 @@
+<?php
+
+    $text = "The quick brown fox jumps over the lazy dog.";
+
+    function modifieText($text) {
+        $lower_Text = strtolower($text);
+        $modifiedText = str_replace("brown", "red", $lower_Text);
+    
+        echo $modifiedText;
+    }
+    
+    modifieText($text);
